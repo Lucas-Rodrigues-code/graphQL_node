778a3ce8882cb296c43ecebf3cd591d098b79792
lucas <Rodriguesdesouzal28@gmail.com>
2024-08-04 09:53:07 -0300

    model

--- src/resolvers/appointments-resolver.ts
+++ src/resolvers/appointments-resolver.ts
@@ -1,12 +1,19 @@
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
 import { CreateAppointmentInput } from "../dtos/inputs/create-appointment";
+import { Appointment } from "../dtos/models/appointment-model";
 
 @Resolver()
 export class AppointmentsResolver {
-  @Query(() => String)
-  async hello() {
-    return "Hello, world!";
+  @Query(() => [Appointment])
+  async appointments() {
+    return [];
   }
-  @Mutation(() => String)
-  async createAppointment(@Arg("data") data: CreateAppointmentInput) {}
+  @Mutation(() => Appointment)
+  async createAppointment(@Arg("data") data: CreateAppointmentInput) {
+    const appointment = {
+      startAt: data.startAt,
+      endsAt: data.endsAt,
+    };
+    return appointment;
+  }
 }
